73aaf968f7bacf023b729ad3043df101fcf2ad7f
Cadence K <cadencehiggins@gmail.com>
2024-03-27 21:12:13 -0500

    finished CSS, added JavaScript for post format

---
+++ posts/script.js
@@ -0,0 +1,45 @@
+const postsContainer = document.getElementById('main')
+const postsEl = document.createElement('div')
+postsEl.classList.add('post')
+const title = document.createElement('h2')
+title.textContent = 'My Title';
+const body = document.createElement('p')
+body.textContent = 'body!!!!!!!!!!!!';
+const author = document.createElement('p')
+author.textContent = 'Posted by: Cadence';
+
+postsEl.appendChild(title);
+postsEl.appendChild(body);
+postsEl.appendChild(author);
+postsContainer.appendChild(postsEl)
+
+
+
+console.log(postsEl)
+const post = {
+    title: 'Post Title',
+    body: 'Body',
+    author: 'Posted by: Cadence',
+    createdAt: '3/27/2024',
+}
+
+let posts = [{ title: 'weiner', body: 'boob', author: 'posted by me' }, { title: 'pinot', body: 'is a poo head', author: 'posted by pinots biggest hater' }, { title: '312313', body: 'b3333ob', author: 'posted551412 by me' }, post]
+
+for (let i = 0; i < posts.length; i++) {
+    const currentPost = posts[i]
+    const postsContainer = document.getElementById('main')
+    const postsEl = document.createElement('div')
+    postsEl.classList.add('post')
+    const title = document.createElement('h2')
+    title.textContent = currentPost.title;
+    const body = document.createElement('p')
+    body.textContent = currentPost.body
+    const author = document.createElement('p')
+    author.textContent = currentPost.author;
+
+    postsEl.appendChild(title);
+    postsEl.appendChild(body);
+    postsEl.appendChild(author);
+    postsContainer.appendChild(postsEl)
+
+}
